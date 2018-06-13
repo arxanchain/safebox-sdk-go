fd64eddbb1513078af80db0884f09b1b551d5f13
zhangbing <zhangbing@arxanfintech.com>
2018-06-13 17:53:13 +0800

    [T3973] init safebox sdk project
    Change-Id: Ifb0b65d082291c6f93b8c4aa1f4907fa5c0b06fa

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
+<<<<<<< HEAD
 # safebox-sdk-go
 This SDK enables Go developers to develop applications that interact with Arxanchain SaveBox
+=======
+# Status
+
+# safebox-go-sdk
+
+# Usage
+
+## Install
+>>>>>>> 36af320... [T3973] init safebox sdk project
---
+++ scripts/goUnitTests.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#
+# Copyright Greg Haskins All Rights Reserved.
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+set -e
+
+export GO15VENDOREXPERIMENT=1
+echo -n "Obtaining list of tests to run.."
+PKGS=`go list github.com/arxanchain/safebox-sdk-go/... | grep -v /vendor/`
+echo "DONE!"
+
+echo "Running tests..."
+go test -cover -p 1 -timeout=20m $PKGS
